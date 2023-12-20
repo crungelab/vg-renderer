ebac0cda7a3f5f612c34b81c0bc344d2b7a84595
kfields <kurtisfields@gmail.com>
2023-12-20 03:54:40 -0700

    update

--- src/libs/fontstash.cpp
+++ src/libs/fontstash.cpp
@@ -1,5 +1,5 @@
 #include <bx/bx.h>   // BX_UNUSED
 #include <bx/math.h> // bx::exp
 #include <limits.h>  // INT_MIN, INT_MAX
-#define FONTSTASH_IMPLEMENTATION
+//#define FONTSTASH_IMPLEMENTATION
 #include "fontstash.h"
